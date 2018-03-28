a84f756a1d4832ffc41e3edc582cd4f4dd741375
clintonsoares <37863276+clintonsoares@users.noreply.github.com>
2018-03-28 17:11:22 +0530

    Update Pizza_Customer.java

--- src/pizzaOrder/Pizza_Customer.java
+++ src/pizzaOrder/Pizza_Customer.java
@@ -35,23 +35,32 @@ public class Pizza_Customer {
 		 double gst_amt;
 		 double fin_amt;
 		 Enumeration<Pclass> pizzas = Collections.enumeration(_pizzas);
-		 String result = "Final bill for customer " + getName() + "\n";
+		 System.out.println();
+			System.out.println("--------------------------------------Smoking Joe's Pizza Palace--------------------------------------");
+			System.out.println();
+			System.out.println("	Pizza Item	" + " 		Type " +  "     	Price	"+"	Quantity	"+ "Total Price	");
+			System.out.println("......................................................................................................");
+		 String result = "\n\tCustomer Name: " + getName() + "\n\n";
 		
 		 while(pizzas.hasMoreElements())
 		 {
 			 double thisAmount = 0;
 			 Pclass each = (Pclass) pizzas.nextElement();
 			 thisAmount = each.getPizza().getPizza_amt() * each.getPizza().getQuantity();
-			 result += "\t" + each.getPizza().getPizza_name()+ "\t" + each.getPizza().getPizza_type() + "\t" + each.getPizza().getPizza_amt() + "\t" + each.getPizza().getQuantity() + "\t" + 
+			 result += "\t" + each.getPizza().getPizza_name()+ "\t" + each.getPizza().getPizza_type() + "\t" + each.getPizza().getPizza_amt() + "\t\t" + each.getPizza().getQuantity() + "\t\t" + 
 					 String.valueOf(thisAmount) + "\n";
 			  totalAmount += thisAmount;	 
 		 }
-		 result +=  "Total Amount :" + String.valueOf(totalAmount) + "\n";
-		 System.out.println("GST is 8 % of total amount");
+		 result+="......................................................................................................";
+		 result +=  "\n\tTotal Amount :" + String.valueOf(totalAmount) + "\n";
+		 result+="......................................................................................................";
+		 result +="\n\tGST is 8 % of total amount";
 		 gst_amt = totalAmount * gst;
-		 System.out.println("GST = "+ gst_amt);
+		 result +="\n\tGST = "+ gst_amt;
 		 fin_amt = totalAmount + gst_amt;
-		 result +=  "Final Amount :" + String.valueOf(fin_amt) + "\n";
+		 result+="\n......................................................................................................";
+		 result +=  "\n\n\tFinal Amount :" + String.valueOf(fin_amt) + "\n";
+		 result+="------------------------------------------------------------------------------------------------------";
 		 return result;
 	 }
 	  
