71e87ba2ea2ea7322d9c0a353b007aaa72c3707d
Clinton <Clinton@DESKTOP-BDDUAEN>
2018-03-28 16:48:51 +0530

    Pizza Order System //Lab Isa 1

---
+++ src/pizzaOrder/Pizza_CustomerTest.java
@@ -0,0 +1,27 @@
+package pizzaOrder;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Pizza_CustomerTest {
+	
+	Pizza p1,p2;
+	Pizza_Customer c1;
+
+	@Before
+	public void setUp() throws Exception {
+		
+		p1 = new Pizza ("Smokin Grilled Chicken","Non-Veg",250,3);
+		p2 = new Pizza ("French Bread with capsicum","Veg",225,1);
+		c1 = new Pizza_Customer("Clinton");
+		c1.addPizza(new Pclass(p1));
+		c1.addPizza(new Pclass(p2));
+		System.out.println(c1.bill());
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
